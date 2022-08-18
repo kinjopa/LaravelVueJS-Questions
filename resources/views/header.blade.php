a225b64a0762cf5668bf7ecd54f2e7c6ac172a68
DESKTOP-BEMFP5F\Sergey <s.shevtsov@subbclub.ru>
2022-08-18 12:48:04 +0500

    add auth/reg, style main page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -7,67 +7,40 @@
                  id="navcol-1">
                 <ul class="nav navbar-nav">
                     <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Мои вопросы</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Популярные вопросы</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Популярные вопросы</a>
+                    </li>
                 </ul>
                 <form class="form-inline mr-auto" target="_self">
                     <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label><input
                             class="form-control search-field" type="search" name="search" id="search-field"></div>
                 </form>
-                <span class="navbar-text"> <a href="{{ route('login') }}" class="login">Вход</a></span><a
-                    class="btn btn-light action-button" role="button" href="{{ route('register') }}">Регистарция</a></div>
+                @if(Auth::check())
+                    <a title="Profile"
+                       class="menuBar_item menuBar_item-profile d-flex flex-col flex-items-center justify-end lh-spacy"
+                       href="https://ask.fm/totsamiy312">
+                        <img class="userAvatar" loading="lazy"
+                             src="https://casts.ask.fm/assets/noAvatar-2325eb51f8abe4e4678a25b16cb32a5399e84d9e82b5bc7efcc0b623c2aa12ec.png"/>
+                        <p class="username ellipsis">{{ Auth::user()->name }}</p>
+
+                    </a>
+                    <li class="dropdown"><a class="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown"
+                                            aria-expanded="false" href="#"
+                                            style="color: red;font-size: 19px;margin-top: 5px"></a>
+                        <div class="dropdown-menu" role="menu">
+                            <a class="dropdown-item" role="presentation" href="#" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();  document.getElementById('logout-form').submit();">Выход</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @else
+                    <span class="navbar-text"> <a href="{{ route('login') }}" class="login">Вход</a></span>
+                    <a class="btn btn-light action-button" role="button" href="{{ route('register') }}">Регистарция </a>
+            </div>
+            @endif
         </div>
+
     </nav>
 </div>
 
-<!--<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            &lt;!&ndash; Left Side Of Navbar &ndash;&gt;
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            &lt;!&ndash; Right Side Of Navbar &ndash;&gt;
-            <ul class="navbar-nav ml-auto">
-                &lt;!&ndash; Authentication Links &ndash;&gt;
-                @guest
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                    @if (Route::has('register'))
-        <li class="nav-item">
-            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-@else
-    <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-{{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-        </form>
-    </div>
-</li>
-@endguest
-    </ul>
-</div>
-</div>
-</nav>-->
